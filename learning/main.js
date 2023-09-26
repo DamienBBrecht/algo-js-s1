bf8753d9f8f91a75c7e941a3aa1dbeac30bd5153
DamienBBrecht <146093947+DamienBBrecht@users.noreply.github.com>
2023-09-26 18:43:44 +0200

    essaie javascripte damienbbrecht

---
+++ learning/main.js
@@ -0,0 +1,17 @@
+alert('Mon fils, que jesus te garde si tu quittes cette page.')
+
+console.log("Je suis mort")
+console.log("J'ai ensuite réssucité pour chatié les incultes qui respire sur cette terre.")
+
+var cardName="Antichrist"
+let age = 666
+
+const teacherAge = 68
+const studentAge = 13
+if(teacherAge< studentAge){
+    console.log('true')
+}
+else {
+    console.log('fakegod')
+
+}
