b94454cab8b05494e0191d242498acb9276e68e6
DamienBBrecht <146093947+DamienBBrecht@users.noreply.github.com>
2023-09-26 17:37:43 +0200

    exercices 1&2, cours d'algorithme

---
+++ exercice 1/main.js
@@ -0,0 +1,18 @@
+var a=4
+var b=4
+var c=3
+
+if (a===b) {
+    console.log("c'est égale")
+}
+else{console.log("eroro404")}
+
+if (c<b && a+c !==3) { 
+    console.log("ok boufi")
+
+}
+else{
+    console.log("echekk")
+
+}
+    
